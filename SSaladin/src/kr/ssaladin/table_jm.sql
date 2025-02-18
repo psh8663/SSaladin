081468a2015c1e7b5895987f1a3e9b74d394191c
SIST <SIST@DESKTOP-J2V152O>
2025-02-18 17:11:16 +0900

    reviews, request table

---
+++ SSaladin/src/kr/ssaladin/table_jm.sql
@@ -0,0 +1,20 @@
+create table reviews (
+	review_num number primary key,
+	user_id varchar2(12) not null,
+	book_code number not null,
+	rating number(1) not null,
+	reg_date date default SYSDATE not null,
+	foreign key (user_id) references users (user_id)
+);
+
+create sequence reviews_seq;
+
+create table request (
+	request_num number primary key,
+	user_id varchar2(12) not null,
+	request_content varchar2(300) not null,
+	request_date date not null,
+	foreign key (user_id) references users (user_id)
+);
+
+create sequence request_seq; 
