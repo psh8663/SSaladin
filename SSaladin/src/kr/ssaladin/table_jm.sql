1b4da40dc3bfe931dfd7ac29beec020d0aadb67a
SIST <SIST@DESKTOP-J2V152O>
2025-02-18 17:36:38 +0900

    reviews, request 테이블 생성

--- SSaladin/src/kr/ssaladin/table_jm.sql
+++ SSaladin/src/kr/ssaladin/table_jm.sql
@@ -4,7 +4,7 @@ create table reviews (
 	book_code number not null,
 	rating number(1) not null,
 	reg_date date default SYSDATE not null,
-	foreign key (user_id) references users (user_id)
+	foreign key (user_id) references users (user_id) on delete cascade
 );
 
 create sequence reviews_seq;
@@ -14,7 +14,7 @@ create table request (
 	user_id varchar2(12) not null,
 	request_content varchar2(300) not null,
 	request_date date not null,
-	foreign key (user_id) references users (user_id)
+	foreign key (user_id) references users (user_id) on delete cascade
 );
 
-create sequence request_seq; 
+create sequence request_seq;
